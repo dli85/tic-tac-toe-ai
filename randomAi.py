a2162478e856a31038cae86859ceb98f3cf2a1b9
dli85 <53060191+dli85@users.noreply.github.com>
2020-08-25 20:49:36 -0400

    Create randomAi.py

---
+++ randomAi.py
@@ -0,0 +1,20 @@
+import random
+
+class RandomAI:
+    def __init__(self, playerNum):
+        self.playerNum = playerNum
+
+    def selectMove(self, board):
+        states = []
+        for r in board:
+            for c in r:
+                states.append(c)
+
+        availableMoves = []
+        for i in range(len(states)):
+            if states[i] == 0:
+                availableMoves.append(i + 1)
+
+        return availableMoves[random.randint(0, len(availableMoves) - 1)]
+
+
