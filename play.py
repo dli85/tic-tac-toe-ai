690ef0c448f8b58c141adcede2121d3951315cf7
dli85 <53060191+dli85@users.noreply.github.com>
2020-08-26 15:43:51 -0400

    Add minimax

--- play.py
+++ play.py
@@ -1,11 +1,12 @@
 from randomAi import RandomAI
 from game import Game
+from minimaxAi import minimaxAI
 
 def playerTurn():
     pos = int(input('Where would you like to play? '))
     return pos
 
-aiNum = int(input('What ai would you like to play against? (1 = random): '))
+aiNum = int(input('What ai would you like to play against? (1 = random, 2 = minimax): '))
 
 if aiNum == 1:
     currentTurn = 1
@@ -32,5 +33,30 @@ if aiNum == 1:
         print("computer won")
     else:
         print("its a tie")
+elif aiNum == 2:
+    currentTurn = 1
+    g = Game()
+    agent = minimaxAI(2)
+    while g.checkWinner() == -1:
+        g.printBoard()
+        if currentTurn == 1:
+            pos = playerTurn()
+            g.playMove(pos, 1)
+            currentTurn = 2
+        else:
+            pos = agent.getMove(g.board)
+            g.playMove(pos, 2)
+            currentTurn = 1
+            print('Ai has played at position', pos)
+
+    g.printBoard()
+
+    if g.checkWinner() == 1:
+        print("You won!")
+    elif g.checkWinner() == 2:
+        print("computer won")
+    else:
+        print("its a tie")
+
 
 
