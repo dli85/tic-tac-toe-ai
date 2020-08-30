4fd1286e27f13451ea3363ddb8eb998e1470a782
dli85 <53060191+dli85@users.noreply.github.com>
2020-08-30 11:46:32 -0400

    ADD Neural Network Supervised

--- play.py
+++ play.py
@@ -1,12 +1,13 @@
 from randomAi import RandomAI
 from game import Game
 from minimaxAi import minimaxAI
+from NNS import NeuralNetworkSupervised
 
 def playerTurn():
     pos = int(input('Where would you like to play? '))
     return pos
 
-aiNum = int(input('What ai would you like to play against? (1 = random, 2 = minimax): '))
+aiNum = int(input('What ai would you like to play against? (1 = random, 2 = minimax 3 = Neural Network Supervised Learning): '))
 
 if aiNum == 1:
     currentTurn = 1
@@ -63,8 +64,32 @@ elif aiNum == 2:
         print("computer won")
     else:
         print("its a tie")
+elif aiNum == 3:
+    currentTurn = 1
+    g = Game()
+    agent = NeuralNetworkSupervised(2)
+    agent.loadModel('NNRand.h5')
 
+    while g.checkWinner() == -1:
+        g.printBoard()
+        if currentTurn == 1:
+            pos = playerTurn()
+            while not g.legalMove(pos):
+                print('Not a legal move, please choose again')
+                pos = playerTurn()
+            g.playMove(pos, 1)
+            currentTurn = 2
+        else:
+            pos = agent.getMove(g.board)
+            g.playMove(pos, 2)
+            currentTurn = 1
+            print('Ai has played at position', pos)
 
+    g.printBoard()
 
-
-
+    if g.checkWinner() == 1:
+        print("You won!")
+    elif g.checkWinner() == 2:
+        print("computer won")
+    else:
+        print("its a tie")
