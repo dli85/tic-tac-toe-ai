1a45ae5306806d3324f34332a1cc92d716aee401
David Li <53060191+dli85@users.noreply.github.com>
2020-10-27 13:16:38 -0400

    Update matches

--- gameResultCollector.py
+++ gameResultCollector.py
@@ -37,39 +37,6 @@ def NNRp1RandP2():
 
     print('[P1: minimax, P2: rand] Player 1 won: ' + str(player1WinCount) + ' games, player 2 won ' + str(player2WinCount) + ' games, it was a draw ' + str(drawCount) + ' times')
 
-
-def NNRp1MiniP2():
-    player1 = NeuralNetworkReinforcement(1)
-    player2 = minimaxAI(2)
-
-    player1WinCount = 0
-    player2WinCount = 0
-    drawCount = 0
-
-    for i in tqdm(range(100)):
-        g = Game()
-        currentPlayer = 1
-
-        while g.checkWinner() == -1:
-            if currentPlayer == 1:
-                pos = player1.getMove(g.board)
-                g.playMove(pos, 1)
-                currentPlayer = 2
-            else:
-                pos = player2.getMove(g.board)
-                g.playMove(pos, 2)
-                currentPlayer = 1
-
-        if g.checkWinner() == 1:
-            player1WinCount += 1
-        elif g.checkWinner() == 2:
-            player2WinCount += 1
-        else:
-            drawCount += 1
-
-    print('[P1: Neural Net Reinforcement, P2: Minimax] Player 1 won: ' + str(player1WinCount) + ' games, player 2 won ' + str(player2WinCount) + ' games, it was a draw ' + str(drawCount) + ' times')
-
-
 def MiniP1RandP2():
     player1 = minimaxAI(1)
     player2 = RandomAI(2)
@@ -171,7 +138,6 @@ def RandP1NNSP2():
 
 if __name__ == '__main__':
     NNRp1RandP2()
-    NNRp1MiniP2()
     RandP1NNSP2()
     MiniP1RandP2()
     RandP1RandP2()
